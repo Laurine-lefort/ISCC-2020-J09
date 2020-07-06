d93fd50af6fd99bd750b6fc8f8802a0d268a8f7d
Laurine-lefort <laurine.le-fort@iseg.fr>
2020-07-06 17:18:57 +0200

    jour 9

--- EX-01/mini-site-routing.php
+++ EX-01/mini-site-routing.php
@@ -9,6 +9,7 @@
   <a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=2">Page 1</a>
   <a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/mini-site-routing.php?page=3">Page 2</a>
   <a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/connexion.php?page=connexion">Page connexion</a>
+  <a href="http://localhost:8888/ISCC-2020/ISCC-2020-J09/EX-01/admin.php?page=admin">Page admin</a>
     <?php
     session_start();
     if($_GET['page'] == 1){
@@ -24,6 +25,10 @@
     elseif($_GET ['page'] =='connexion'){
         echo'<h1>Page connexion</h1>';
         include ("connexion.php");
+    }
+    elseif($_GET['page'] =='admin'){
+        echo'<h1>Page admin</h1>';
+        include ("admin.php");
     }
     ?>
 <?php
